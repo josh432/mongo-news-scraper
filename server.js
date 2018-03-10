967c263d79c11ee5cb3c4885d192b0f84068a56d
Josh Silverman <jrsilverman71@hotmail.com>
2018-03-09 19:02:35 -0800

    middleware/mongoose/herokudb setup

--- server.js
+++ server.js
@@ -1,8 +1,33 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const exphbs  = require('express-handlebars');
+const request = require('request');
+
 const app = express();
+
 
 const PORT = process.env.PORT || 8080;
 
+app.use(express.static('public'));
+
+// Mongoose connection
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
+mongoose.Promise = Promise;
+mongoose.connect(MONGODB_URI, {
+  useMongoClient: true
+});
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+
+// Middleware: Body-Parser
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+// Middleware: Express-Handlebars
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
+
 app.get('/', (req, res) => res.send('Hello World!'));
 
-app.listen(PORT, () => console.log(`News Scrapper listening on port ${PORT}`));
+app.listen(PORT, () => console.log(`News Scraper listening on port ${PORT}`));
